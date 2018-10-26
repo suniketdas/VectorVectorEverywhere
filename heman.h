737c4f14ce361d4a145694336f5332c476b75894
hemanvaghasiya <vaghasiya.1@iiyj.ac.in>
2018-10-27 01:47:20 +0530

    added sum dot print

--- heman.h
+++ heman.h
@@ -1,1 +1,22 @@
 //heman header file
+
+  struct vector sum(struct vector a,struct vector b)
+ {    
+ 	struct vector c={a.i-b.i,a.j-b.j,a.k-b.k}
+ 	return c;
+ }
+ struct vector sum(struct vector a,struct vector b)
+ {    
+ 	struct vector c={a.i+b.i,a.j+b.j,a.k+b.k}
+ 	return c;
+ }
+
+ int dotproduct(struct vector a,struct vector b)
+    {
+         return (a.i)*(b.i)+(a.j)*(b.j)+(a.k)*(b.k);
+    }
+
+void print(struct vector a)
+{
+	printf("\n%di + %dj + %dk",a.i,a.j,a.k);
+}
