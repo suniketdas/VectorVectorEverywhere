cd8a257a9805eca7f8050ef9e7d7f91c6d751fb0
suniketdas <das.5@iitj.ac.in>
2018-10-27 01:55:34 +0530

    made cross

--- suniket.h
+++ suniket.h
@@ -1,1 +1,7 @@
 //This is Suniket's header file
+struct vector cross(struct vector a, struct vector b)
+{
+	struct vector c;
+	c={((a.2*b.3)-(a.3*b.2)),((a.3-b.1)-(a.1*b.3)),((a.1*b.2)-(a.2*b.1))};
+	return c;
+};
