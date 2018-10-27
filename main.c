3149b18b5b63cf72df66efd854ac060c4e487a79
NiveditJain <niveditjain@outlook.com>
2018-10-27 09:28:43 +0530

    created menu

--- main.c
+++ main.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include "heman.h"
+#include "suniket.h"
 struct vector{
 	int i;
 	int j;
@@ -6,5 +8,11 @@ struct vector{
 };
 int main()
 {
-	
+	struct vector a,b,c;
+	printf("\n\t\t\t\t\t*******Welcome To vector calculator********\n\n");
+	int draw,flag=1;
+	do
+	{
+		printf("\n Please enter your 2 vector");
+	}
 }
